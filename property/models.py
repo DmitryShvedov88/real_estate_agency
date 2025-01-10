2590f60fd7ec4efbdb19f13c6a143c3cbe7a0017
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-01-10 21:24:58 +0300

    Исправил Релетед найм

--- property/models.py
+++ property/models.py
@@ -52,7 +52,7 @@ class Flat(models.Model):
     likes = models.ManyToManyField(
         User,
         verbose_name='Лайк',
-        blank=True, related_name="likes"
+        blank=True, related_name="likes_on_flat"
         )
 
     def __str__(self):
@@ -61,15 +61,15 @@ class Flat(models.Model):
 
 class Complaints(models.Model):
     '''Complaints class'''
-    author = models.ForeignKey(
+    user = models.ForeignKey(
         User,
         verbose_name="Пользователь, который жалуется",
         on_delete=models.CASCADE,
-        null=True, blank=True, related_name="user"
+        null=True, blank=True, related_name="user_сomplaints"
         )
     flat = models.ForeignKey(
         Flat, verbose_name="Квартира, на которую жалуются", on_delete=models.CASCADE,
-        null=True, blank=True, related_name="flat"
+        null=True, blank=True, related_name="complaint_on_flat"
         )
     description = models.TextField(
         null=True, blank=True, verbose_name="Текст жалобы"
@@ -92,7 +92,7 @@ class Owner(models.Model):
         )
     flats = models.ManyToManyField(
         Flat, verbose_name="Квартира, кототорая продается",
-        null=True, related_name="flat"
+        null=True, related_name="owner_flat"
         )
 
     def __str__(self):
